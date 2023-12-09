f1b58049975e670cb44d72bc12b1b0a037cbc05a
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2023-12-09 14:53:32 -0800

    add toppings

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ function Pizza(toppings, size) {
     this.toppings = toppings;
     this.size = size;
 }
+function createCustomization() {
+    const div = document.createElement("div");
+    div.classList.add("customization");
+    const 
+}

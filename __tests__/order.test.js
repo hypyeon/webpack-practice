1adf5976da02353ce6c4970cd7ca6f562b555515
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2023-12-13 10:34:14 -0800

    first test passed

---
+++ __tests__/order.test.js
@@ -0,0 +1,13 @@
+import Order from '../src/order.js';
+
+describe('Order', () => {
+    test('should correctly create an order object with five properties', () => {
+        const order = new Order("Lucy", "small", ["mushroom", "onion"], ["pepperoni"], "pickup");
+        expect(order.orderName).toEqual("Lucy");
+        expect(order.size).toEqual("small");
+        expect(order.firstToppings).toEqual(["mushroom", "onion"]);
+        expect(order.secondToppings).toEqual(["pepperoni"]);
+        expect(order.method).toEqual("pickup");
+    })
+})
+

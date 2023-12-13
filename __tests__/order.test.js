6020850ea4346f7221ba7b587ab0f838f866ab6e
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2023-12-13 10:53:48 -0800

    fourth test passed

--- __tests__/order.test.js
+++ __tests__/order.test.js
@@ -23,10 +23,20 @@ describe('Order', () => {
     test('should return a number with two decimals which is a sum of prices of selected pizza size, number of toppings, and delivery method', () => {
         const order = new Order("Lucy", "small", ["mushroom", "onion"], ["pepperoni"], "delivery");
         expect(order.getTotalPrice()).toEqual("16.50");
-    })
+    });
 
     test('should apply discount of 5% when selected "pickup" for delivery method', () => {
         const order = new Order("Lucy", "small", ["mushroom", "onion"], ["pepperoni"], "pickup");
         expect(order.getTotalPrice()).toEqual("15.67");
-    })
+    });
+
+    test('should return "Yes" when "pickup" is selected as delivery method', () => {
+        const order = new Order("Lucy", "small", ["mushroom", "onion"], ["pepperoni"], "pickup");
+        expect(order.discountEligibility()).toEqual("Yes");
+    });
+
+    test('should return "No" when "pickup" is not selected as delivery method', () => {
+        const order = new Order("Lucy", "small", ["mushroom", "onion"], ["pepperoni"], "delivery");
+        expect(order.discountEligibility()).toEqual("No");
+    });
 });
